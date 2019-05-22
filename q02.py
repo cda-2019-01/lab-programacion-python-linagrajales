e8ab550200384ab7128550efb58cd97208cc73b7
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-22 18:22:20 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -8,3 +8,22 @@
 ## D,6
 ## E,14
 ##
+import numpy as np
+import csv
+file = open('data.csv', 'r')
+
+col0=[]; col1=[]; col2=[]; col3=[]; col4=[]; 
+
+for line in file:    
+    data = line.strip().split("\t")            
+    col0.append(data[0]); 
+    col1.append(float(data[1])); 
+    col2.append(data[2]); 
+    col3.append(data[3]); 
+    col4.append(data[4])
+
+from collections import Counter
+a = "".join(col0)
+charcounts = sorted(Counter(a).most_common())
+for key in charcounts:
+    print(key[0],key[1], sep=",")
