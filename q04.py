10f5c29dcb9fcedd391df0b4f908568895f79358
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-22 18:25:56 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -14,3 +14,22 @@
 ## 11,2
 ## 12,3
 ##
+import numpy as np
+import csv
+file = open('data.csv', 'r')
+
+col0=[]; col1=[]; col2=[]; col3=[]; col4=[]; 
+
+for line in file:    
+    data = line.strip().split("\t")            
+    col0.append(data[0]); 
+    col1.append(float(data[1])); 
+    col2.append(data[2]); 
+    col3.append(data[3]); 
+    col4.append(data[4])
+
+mes = [z.split('-')[1] for z in col2]
+from collections import Counter
+charcounts = sorted(Counter(mes).most_common())
+for key in charcounts:
+    print(key[0], key[1], sep=",")
