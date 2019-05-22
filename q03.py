1e2cbda873bbd39419fcec5f20ec389a85e7efb3
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-22 18:23:33 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -8,3 +8,27 @@
 ## D,23
 ## E,67
 ##
+import numpy as np
+import csv
+file = open('data.csv', 'r')
+
+col0=[]; col1=[]; col2=[]; col3=[]; col4=[]; 
+
+for line in file:    
+    data = line.strip().split("\t")            
+    col0.append(data[0]); 
+    col1.append(float(data[1])); 
+    col2.append(data[2]); 
+    col3.append(data[3]); 
+    col4.append(data[4])
+    
+    col0join = "".join(col0)
+charcounts = sorted(Counter(col0join).most_common())
+    
+for i in range(0,len(charcounts)):    
+    
+    sum = 0
+    for j, val in enumerate(col0):
+        if(charcounts[i][0]==val):
+            sum+=col1[j]
+    print(charcounts[i][0]+","+str(int(sum))) 
