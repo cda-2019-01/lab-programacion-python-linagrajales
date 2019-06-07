8b35a512c016eea788953c2d947f3e3aa449953a
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-06-07 08:23:38 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -11,3 +11,22 @@
 ## E,2,3
 ## E,3,3
 ##
+import numpy as np
+from collections import Counter
+import csv
+
+file = open('data.csv', 'r')
+
+col0=[]; col1=[]; col2=[]; col3=[]; col4=[]; 
+
+for line in file:
+    
+    data = line.strip().split("\t")            
+    col0.append(data[0]); 
+    col1.append(float(data[1])); 
+    col2.append(data[2]); 
+    col3.append(data[3]); 
+    col4.append(data[4])    
+    
+for i in range(len(col4)):
+    print(str(col0[i])+","+str(len(col3[i].split(',')))+","+str(len(col4[i].split(','))))
