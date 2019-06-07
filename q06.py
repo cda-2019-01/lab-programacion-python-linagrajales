355653e9b8da1054e6760fd897e7eb8ec83099e6
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-06-07 08:11:06 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -13,3 +13,42 @@
 ## iii,2,7
 ## jjj,2,5
 ##
+import numpy as np
+from collections import Counter
+import csv
+
+file = open('data.csv', 'r')
+
+col0=[]; col1=[]; col2=[]; col3=[]; col4=[]; 
+
+for line in file:
+    
+    data = line.strip().split("\t")            
+    col0.append(data[0]); 
+    col1.append(float(data[1])); 
+    col2.append(data[2]); 
+    col3.append(data[3]); 
+    col4.append(data[4])    
+    
+chars = []
+vals = []
+tuples = []
+
+for i, val1 in enumerate(col4):
+    tuples1 = val1.split(",")
+    for j, val2 in enumerate(tuples1):
+        tuples2 = val2.split(":")
+        #print(tuples2)
+        tuples.append(tuples2)
+        chars.append(tuples2[0])
+        vals.append(tuples2[1])
+        
+uniquechars = sorted(Counter(chars).most_common())
+
+for i, vals1 in enumerate(uniquechars):
+    x = np.array([])
+    for j, vals2 in  enumerate(tuples):
+        if(vals1[0]==vals2[0]):
+            #print(vals2[1])
+            x = np.append(x,[float(vals2[1])],axis=0)
+    print(vals1[0]+","+str(int(x.min()))+","+str(int(x.max())))
