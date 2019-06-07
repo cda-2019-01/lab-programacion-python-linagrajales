8853462218648595e214243c007294699d63009e
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-06-07 08:26:22 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -15,3 +15,35 @@
 ## jjj,18
 ##
 ##
+import numpy as np
+from collections import Counter
+import csv
+file = open('data.csv', 'r')
+
+col0=[]; col1=[]; col2=[]; col3=[]; col4=[]; 
+
+for line in file:
+    
+    data = line.strip().split("\t")            
+    col0.append(data[0]); 
+    col1.append(float(data[1])); 
+    col2.append(data[2]); 
+    col3.append(data[3]); 
+    col4.append(data[4])    
+
+chars = []
+vals = []
+tuples = []
+
+for i, val1 in enumerate(col4):
+    tuples1 = val1.split(",")
+    for j, val2 in enumerate(tuples1):
+        tuples2 = val2.split(":")
+        #print(tuples2)
+        tuples.append(tuples2)
+        chars.append(tuples2[0])
+        vals.append(tuples2[1])
+        
+uniquechars = sorted(Counter(chars).most_common())
+for key in uniquechars:
+    print(key[0],key[1], sep=",")
