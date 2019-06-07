5dc3ba24d7a49fd335916f7106eca8e6292110a8
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-06-07 08:20:41 -0500

    Update q13.py

--- q13.py
+++ q13.py
@@ -10,3 +10,30 @@
 ## E,11
 ## E,16
 ##
+import numpy as np
+from collections import Counter
+import csv
+
+file = open('data.csv', 'r')
+
+col0=[]; col1=[]; col2=[]; col3=[]; col4=[]; 
+
+for line in file:
+    
+    data = line.strip().split("\t")            
+    col0.append(data[0]); 
+    col1.append(float(data[1])); 
+    col2.append(data[2]); 
+    col3.append(data[3]); 
+    col4.append(data[4])   
+    
+tuples = []
+
+for i, val1 in enumerate(col4):
+    tuples1 = val1.split(",")
+    x = np.array([])
+    for j, val2 in enumerate(tuples1):
+        tuples2 = val2.split(":")
+        tuples.append(tuples2)
+        x = np.append(x,[float(tuples2[1])],axis=0)
+    print(str(col0[i])+","+str(int(x.sum())))
